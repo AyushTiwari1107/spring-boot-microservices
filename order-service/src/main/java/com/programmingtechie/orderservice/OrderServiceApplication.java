12a90433b3ced44cb7609c79588ef8b88557ceaf
AyushTiwari1107 <134467942+AyushTiwari1107@users.noreply.github.com>
2024-02-21 11:23:24 +0530

    Update OrderServiceApplication.java

--- order-service/src/main/java/com/programmingtechie/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/OrderServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient // this it the latest updated annotation in place the @EnableEurekaClient apply this n all the updated
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
